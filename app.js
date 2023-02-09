d96790a4af260ec23680bd0937478b2518ebd5c4
midCenturyDude7 <mgriffes@gmail.com>
2023-02-08 16:37:09 -0800

    Created HTML layout, styled CSS and included logic via JS

---
+++ app.js
@@ -0,0 +1,30 @@
+const id = (id) => document.getElementById(id);
+const classes = (classes) => document.getElementsByClassName(classes);
+
+const username = id("username"),
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(username, 0, "Username cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(password, 2, "Password cannot be blank");
+});
+
+const engine = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
