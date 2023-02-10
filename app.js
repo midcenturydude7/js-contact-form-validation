71d3e64288811924f9e86d9e03d1ada64a6044bc
midCenturyDude7 <mgriffes@gmail.com>
2023-02-09 20:35:30 -0800

    Working through email validation and success msg trigger

--- app.js
+++ app.js
@@ -7,24 +7,49 @@ const username = id("username"),
   form = id("form"),
   errorMsg = classes("error"),
   successIcon = classes("success-icon"),
-  failureIcon = classes("failure-icon");
+  failureIcon = classes("failure-icon"),
+  // confirm = classes("confirm");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   engine(username, 0, "Username cannot be blank");
-  engine(email, 1, "Email cannot be blank");
+  validateEmail(
+    email,
+    1,
+    "You have entered an invalid email address. Please try again"
+  );
   engine(password, 2, "Password cannot be blank");
 });
 
-const engine = (id, serial, message) => {
+const engine = (id, num, message) => {
   if (id.value.trim() === "") {
-    errorMsg[serial].innerHTML = message;
-    failureIcon[serial].style.opacity = "1";
-    successIcon[serial].style.opacity = "0";
+    errorMsg[num].innerHTML = message;
+    failureIcon[num].style.opacity = "1";
+    successIcon[num].style.opacity = "0";
+    return false;
   } else {
-    errorMsg[serial].innerHTML = "";
-    failureIcon[serial].style.opacity = "0";
-    successIcon[serial].style.opacity = "1";
+    errorMsg[num].innerHTML = "";
+    failureIcon[num].style.opacity = "0";
+    successIcon[num].style.opacity = "1";
   }
 };
+
+const validateEmail = (email, num, message) => {
+  const mailformat = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.value.match(mailformat)) {
+    errorMsg[num].innerHTML = "";
+    failureIcon[num].style.opacity = "0";
+    successIcon[num].style.opacity = "1";
+  } else {
+    errorMsg[num].innerHTML = message;
+    failureIcon[num].style.opacity = "1";
+    successIcon[num].style.opacity = "0";
+  }
+};
+
+// const msgConfirm = (num, message) => {
+//   if (!errorMsg[num].innerHTML !== message) {
+//     confirm.style.opacity = "1";
+//   }
+// };
